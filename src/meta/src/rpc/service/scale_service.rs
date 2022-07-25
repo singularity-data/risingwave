1acb189eb67ee95628dc1214a87dc441938d26b1
Peng Chen <peng@singularity-data.com>
2022-07-25 18:30:04 +0800

    fix conflict
    Signed-off-by: Peng Chen <peng@singularity-data.com>

--- src/meta/src/rpc/service/scale_service.rs
+++ src/meta/src/rpc/service/scale_service.rs
@@ -22,7 +22,7 @@ use risingwave_pb::meta::{
     ResumeResponse,
 };
 
-use risingwave_pb::meta::{MigrateActorRequest, MigrateActorResponse}
+use risingwave_pb::meta::{MigrateActorRequest, MigrateActorResponse};
 
 use tokio::sync::RwLock;
 use tonic::{Request, Response, Status};
